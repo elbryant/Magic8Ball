98df39c0aa2147d6b0c4ee129f5b958210695f3b
Erica Bryant <erica.bryant@me.com>
2017-04-20 20:01:04 -0700

    first javascript project commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var phrases = [
+   "That sounds good.",
+   "Yes, you should definitely do that!",
+   "I'm not sure that's a great idea...",
+   "Maybe not today.",
+   "Computer says no."
+];
+
+phrases[Math.floor(Math.random() * 5)];
